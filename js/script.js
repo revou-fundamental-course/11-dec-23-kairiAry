5996af0d5aae7cb257dea63a7d320079989c261d
kairiAry <hairiamrullah@gmail.com>
2023-12-21 20:27:17 +0800

    menambahkan slideshow image

--- js/script.js
+++ js/script.js
@@ -35,6 +35,31 @@
 // console.log("halo gess")
 // let nama = "";
 
+// MENAMPLKAN GAMBAR
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {slideIndex = 1}  
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3500);
+}
+
+// MENAMPILKAN ALERT
 function menampilkanAlert() {
     var namaPengguna = prompt("Selamat datang! Silakan masukkan nama Anda:");
     if (namaPengguna != null) {
@@ -48,7 +73,7 @@ function menampilkanAlert() {
     }
 }
 
-
+// MENAMPILKAN DATA
 function menampilkanData() {
     var nama = document.getElementById('nama').value;
     var tanggal = document.getElementById('tanggal').value;
@@ -64,11 +89,6 @@ function menampilkanData() {
     document.getElementById('subject').innerHTML = 
     hari + " " + tanggalWaktu + " " + jam + " " + zonaWaktu +
     "\n\nNama :" + nama + "\nTanggal: " + tanggal + "\nJenis Kelamin: " + jenisKelamin + "\nPesan: " + pesan;
-    // "Tanggal :" + tanggal
-    // "Jenis Kelamin :" + jenisKelamin
-    // "Pesan :" + pesan;
-
-    // alert(nama + tanggal + jenisKelamin + pesan)
 
     console.log('Nama: ' + nama);
     console.log('Tanggal: ' + tanggal);
